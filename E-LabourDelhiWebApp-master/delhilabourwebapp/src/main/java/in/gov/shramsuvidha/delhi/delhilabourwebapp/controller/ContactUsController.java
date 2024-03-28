4f96626a2b767aefb2989fb6ad82b43e954ea9a3
anmolwadhwaxx <anmolwadhwa2002@gmail.com>
2024-03-29 00:06:37 +0530

    added files

---
+++ E-LabourDelhiWebApp-master/delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/controller/ContactUsController.java
@@ -0,0 +1,27 @@
+package in.gov.shramsuvidha.delhi.delhilabourwebapp.controller;
+
+import in.gov.shramsuvidha.delhi.delhilabourwebapp.model.ContactUs;
+import in.gov.shramsuvidha.delhi.delhilabourwebapp.model.RegisterPOJO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ContactUsController {
+
+    @RequestMapping(value = "/contactUs", method = RequestMethod.POST)
+    public String contactform(@RequestParam("name") String name,@RequestParam("email") String email, @RequestParam("subject") String sub, @RequestParam("message") String message){
+        //object of contactUs model
+        try{
+            System.out.println("" + email + " test:" + message);
+            ContactUs obj = new ContactUs(name,sub, email, message);
+            //call hibernate to save contact form data
+            System.out.println("" + email + " test:" + message);
+            System.out.println("" + email + " test:" + message);
+            System.out.println("" + email + " test:" + message);
+            return "Data Saved";
+        } catch (Exception e){
+            return "";
+        }
+    }
+
+}
